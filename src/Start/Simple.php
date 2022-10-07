46f5af4bb806084359e75af49d3de48e55e1dba0
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-10-07 14:22:02 +0300

    phpstan level 8 +  folder + namespace update

---
+++ src/Start/Simple.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Saulius\Kh001\Start;
+
+class Simple {
+
+    public function hello(string $word = "Kilo Health Academy") : string {
+        return "Hello world " . $word;
+    }
+}
+
--- tests/Unit/SimpleTest.php
+++ tests/Unit/SimpleTest.php
@@ -2,15 +2,16 @@
 
 namespace Tests\Unit;
 
-use Saulius\Kh001\Simple;
+use Saulius\Kh001\Start\Simple;
 use PHPUnit\Framework\TestCase;
 
 use function PHPUnit\Framework\assertEquals;
 
 final class SimpleTest extends TestCase
 {
-    public function test_if_works() {
-        $x = new Simple;
-        assertEquals($x->hello("KH"), "Hello world KH");
+    public function test_if_works() : void {
+        $simple = new Simple;
+        assertEquals($simple->hello("KH"), "Hello world KH");
+        assertEquals($simple->hello(), "Hello world Kilo Health Academy");
     }
 }
